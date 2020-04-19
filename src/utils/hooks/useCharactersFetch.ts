342f4b1ff83a3ae35c8a1333c1e3e38add45fe59
Bruno Marques <brunomn95@gmail.com>
2020-04-19 12:52:44 -0300

    refactor: refactor custom hooks to use redux

--- src/utils/hooks/useCharactersFetch.ts
+++ src/utils/hooks/useCharactersFetch.ts
@@ -1,24 +1,28 @@
 import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+
+import { fetchCharacters } from 'src/store/character/actions'
 import { Fetch } from 'src/utils/types/fetch'
-import { Character } from 'src/store/character/types'
 import { generateApiKey } from 'src/utils/functions/generateApiKey'
 import { processCharactersApiResult } from 'src/utils/functions/processApiData'
 
 const API_URL = process.env.REACT_APP_API_URL
 
 export const useCharactersFetch = () => {
-  const [result, setResult] = useState<Fetch<Character[]>>({
+  const dispatch = useDispatch()
+  const [result, setResult] = useState<Fetch>({
     status: 'loading',
   })
 
   useEffect(() => {
     fetch(`${API_URL}/characters?limit=48&${generateApiKey()}`)
       .then(response => response.json())
-      .then(response =>
-        setResult({ status: 'loaded', payload: processCharactersApiResult(response.data) }),
-      )
+      .then(response => {
+        dispatch(fetchCharacters(processCharactersApiResult(response.data)))
+        setResult({ status: 'loaded' })
+      })
       .catch(error => setResult({ status: 'error', error }))
-  }, [])
+  }, [dispatch])
 
   return result
 }
--- src/utils/hooks/useSeriesFetch.ts
+++ src/utils/hooks/useSeriesFetch.ts
@@ -1,31 +1,37 @@
 import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+
+import { fetchCharacterSeries } from 'src/store/character/actions'
 import { Fetch } from 'src/utils/types/fetch'
-import { Series } from 'src/store/series/types'
 import { generateApiKey } from 'src/utils/functions/generateApiKey'
 import { processSeriesApiResult } from 'src/utils/functions/processApiData'
 
 const API_URL = process.env.REACT_APP_API_URL
 
 export const useSeriesFetch = (state: any) => {
-  const [result, setResult] = useState<Fetch<Series[]>>({
+  const characterId = state.character.id
+  const dispatch = useDispatch()
+  const [result, setResult] = useState<Fetch>({
     status: 'loading',
   })
 
   useEffect(() => {
     let mounted = true
     if (state) {
-      fetch(`${API_URL}/characters/${state.character.id}/series?limit=20&${generateApiKey()}`)
+      fetch(`${API_URL}/characters/${characterId}/series?limit=20&${generateApiKey()}`)
         .then(response => response.json())
         .then(response => {
-          if (mounted)
-            setResult({ status: 'loaded', payload: processSeriesApiResult(response.data) })
+          if (mounted) {
+            dispatch(fetchCharacterSeries(characterId, processSeriesApiResult(response.data)))
+            setResult({ status: 'loaded' })
+          }
         })
         .catch(error => setResult({ status: 'error', error }))
     }
     return () => {
       mounted = false
     }
-  }, [state])
+  }, [state, characterId, dispatch])
 
   return result
 }
