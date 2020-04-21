26cde130dd4a093840c742db2d82ae210fc5bd4f
Bruno Marques <brunomn95@gmail.com>
2020-04-21 18:48:17 -0300

    test: remove character from test card scope

--- src/components/molecules/card/index.unit.test.tsx
+++ src/components/molecules/card/index.unit.test.tsx
@@ -2,27 +2,16 @@ import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, cleanup, screen } from '@testing-library/react'
 import Card from '.'
-import { Character } from 'src/store/character/types'
 
 afterEach(cleanup)
 
-const character: Character = {
-  id: 0,
-  name: 'Marvel Hero!',
+const card = {
+  title: 'Marvel Hero!',
   image: '',
-  description: 'A fancy Marvel Hero!',
-  series: [],
 }
 
 test('should render a card with image and title', async () => {
-  render(
-    <Card
-      title={character.name}
-      image={character.image}
-      ariaLabel="personagem"
-      onClick={() => {}}
-    />,
-  )
+  render(<Card title={card.title} image={card.image} ariaLabel="personagem" onClick={() => {}} />)
 
-  expect(screen.getByText(character.name)).toBeTruthy()
+  expect(screen.getByText(card.title)).toBeTruthy()
 })
