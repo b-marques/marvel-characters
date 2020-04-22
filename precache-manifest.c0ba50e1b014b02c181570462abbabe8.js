9525d4e352a7f070a0906b2964010c929da15207
Bruno Marques <brunomn95@gmail.com>
2020-04-21 23:26:53 -0300

    Updates

---
+++ precache-manifest.c0ba50e1b014b02c181570462abbabe8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a28b966f9230cf0470b9fa9e2fbda751",
+    "url": "/marvel-characters/index.html"
+  },
+  {
+    "revision": "24a82accd1ff7ad66907",
+    "url": "/marvel-characters/static/css/main.265d603d.chunk.css"
+  },
+  {
+    "revision": "101a8c0ed4b14db60363",
+    "url": "/marvel-characters/static/js/2.838e53c6.chunk.js"
+  },
+  {
+    "revision": "c3e06506fc1a5ff019a01037aa28f646",
+    "url": "/marvel-characters/static/js/2.838e53c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24a82accd1ff7ad66907",
+    "url": "/marvel-characters/static/js/main.aac7adba.chunk.js"
+  },
+  {
+    "revision": "75cf6a45b1d6daaff339",
+    "url": "/marvel-characters/static/js/runtime-main.f937f0ac.js"
+  },
+  {
+    "revision": "288f706112702fd7b2cacc679b4a5821",
+    "url": "/marvel-characters/static/media/logo.288f7061.svg"
+  }
+]);
